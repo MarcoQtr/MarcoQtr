a5d9754ab3c27291e1342bc8d110f0549a9d487d
MarcoQtr <wigleg022king@gmail.com>
2022-03-04 01:53:22 -0700

    Cambio

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,9 @@
 const slider = document.querySelector("#slider");
 let sliderSection = document.querySelectorAll(".slider__section");
-let sliderSectionLast = sliderSectionLast = sliderSection[sliderSection.length -1];
+let sliderSectionLast = sliderSectionLast[sliderSection.length -1]
 
-const btnleft = document.querySelector("#btn-left");
-const btnright = document.querySelector("#btn-right");
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
 
 slider.insertAdjacentElement('afterbegin', sliderSectionLast);
 
@@ -11,7 +11,7 @@ function Next() {
     let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
     slider.style.marginLeft ="-200%";
     slider.style.transition = "all 0.5s";
-    setTimeout(function() {
+    setTimeout(function(){
         slider.style.transition ="none";
         slider.insertAdjecentElement ('beforeend', sliderSectionFirst);
         slider.style.marginLeft = "-100%";
@@ -30,11 +30,11 @@ function Prev() {
     }, 500);
 }
 
-btnright.addEventListener('click', function(){
+btnRight.addEventListener('click', function(){
     Next();
 });
 
-btnleft.addEventListener('click', function(){
+btnLeft.addEventListener('click', function(){
     Prev();
 });
 
