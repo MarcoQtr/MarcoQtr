f303c147f3b0e2e325e497a5331fe8093b3a6306
MarcoQtr <wigleg022king@gmail.com>
2022-03-03 20:47:32 -0700

    Cambio a PHP

--- index.php
+++ index.php
@@ -30,10 +30,28 @@
         <div class="main-header__container">
             <i class="fa-solid fa-address-card"></i>
             <a href="" class="main-header__btn">  Mis compras <i class="fa-solid fa-cart-shopping"></i> </a> 
-            <input type="search"> <i class="fa-solid fa-magnifying-glass"></i> 
+            <input type="search" class="main-header__input" placeholder="       Buscar Productos"> <i class="fa-solid fa-magnifying-glass"></i> 
 
            </div>
         </div>
         </header>
+        <div class="container-slider">
+            <div class="slider" id="slider">
+                <div class="slider__section">
+                    <img src="img/Abarrotes.png" alt="" class="slider__img">
+                </div> 
+                <div class="slider__section">
+                    <img src="img/electros.png" alt="" class="slider__img">
+                </div>
+                <div class="slider__section">
+                    <img src="img/interior-tienda.jpg" alt="" class="slider__img">
+                </div>
+                <div class="slider__section">
+                    <img src="img/limpieza.png" alt="" class="slider__img">
+                </div>
+            </div>
+            <div class="slider__btn slider__btn--right" id="btn-right"> &#62; </div>
+            <div class="slider__btn slider__btn--left" id="btn-left"> &#60; </div>
+        </div>
     </body>
 </html>
